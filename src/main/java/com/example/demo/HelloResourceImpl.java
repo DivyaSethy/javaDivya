e103e01a5e7e6616f5e4a02743be46ca4705ab88
Divya <diya298022sethy@gmail.com>
2022-03-05 20:19:09 +0530

    new version

---
+++ src/main/java/com/example/demo/HelloResourceImpl.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class HelloResourceImpl implements HelloResourceV1{
+
+
+	@Override
+	public String getHelloWorld() {
+		// TODO Auto-generated method stub
+		return "hello world Divya";
+	}
+
+
+
+}
