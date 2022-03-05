66da4b552263a8fc5125434ef44921cc4d23cb23
Divya <diya298022sethy@gmail.com>
2022-03-05 19:01:41 +0530

    my first java project

---
+++ demo/src/main/java/com/example/demo/HelloResourceV1.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+public interface HelloResourceV1 {
+
+	@GetMapping(path = "/hello")
+	public String getHelloWorld(); 
+	
+}
